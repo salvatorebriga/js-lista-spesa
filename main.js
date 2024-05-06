e01e27759614d3fd45b66b4eaad0d61170984bf9
salvatorebriga <salvo.z2302@gmail.com>
2024-05-06 14:58:59 +0200

    added print algorithm

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 "use srict"
+
+const listaDellaSpesa = ['Pane', 'Pasta', 'Pomodori', 'Caramelle', 'Nutella', 'Carne', 'Pesce', 'Pollo', 'Piadina', 'Pesto']
+let i = 0;
+
+const ul = document.getElementById('list');
+
+while(i < listaDellaSpesa.length){
+    const li = document.createElement('li');
+    li.innerText = listaDellaSpesa[i];
+    ul.append(li);
+    i++;
+}
